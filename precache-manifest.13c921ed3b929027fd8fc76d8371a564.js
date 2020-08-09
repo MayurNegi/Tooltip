6893e28c76a77794623bff3de927e88cd6d252ff
Mayur <mayurnegi1999@gmail.com>
2020-08-09 21:20:10 +0530

    Updates

---
+++ precache-manifest.13c921ed3b929027fd8fc76d8371a564.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d5e98ee75be0a99feefb9811c0f92c",
+    "url": "/bigfudge99/Tooltip/index.html"
+  },
+  {
+    "revision": "49c1f0493c3f914485a5",
+    "url": "/bigfudge99/Tooltip/static/css/main.df0c70a2.chunk.css"
+  },
+  {
+    "revision": "e965ece63fcf3cb96101",
+    "url": "/bigfudge99/Tooltip/static/js/2.a0454e1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bigfudge99/Tooltip/static/js/2.a0454e1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49c1f0493c3f914485a5",
+    "url": "/bigfudge99/Tooltip/static/js/main.8e4c260c.chunk.js"
+  },
+  {
+    "revision": "46126a36cc1cbbf1fed2",
+    "url": "/bigfudge99/Tooltip/static/js/runtime-main.99b997c2.js"
+  }
+]);
